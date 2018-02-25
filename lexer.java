aff1f1e95ecca05d9f54754b2d4dbe12d6fff337
vishalmisra235 <34361144+vishalmisra235@users.noreply.github.com>
2018-02-25 23:39:25 +0530

    Add files via upload

---
+++ lexer.java
@@ -0,0 +1,119 @@
+package com.first;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import com.first.tokentype;
+public class lexer  {
+	String[] keywords={"let","as","break","const","continue","crate","else","enum","extern","false","fn","for","if","impl","loop",
+			"match","mod","move","mut","pub","ref","return","Self","self","static","struct","super","main","while","true","Vec","new",
+			"macro","use","text","io","println","read","in"};
+	String[] operators={"+","-","*","/","%","==","!=","<",">","<=",">=","||",".","->","=","!",":","..","#","&&"};
+	String[] seperators={";","(",")","{","}","[","]",","};
+	String[] datatype={"i64","i32","usize","u64","u32"};
+	
+	ArrayList<String> words=new ArrayList<String>();
+	List<String> l1=new ArrayList<String>();
+	
+	public lexer(String code)throws Exception
+	{
+		
+			FileReader fr=new FileReader(code);
+			BufferedReader br=new BufferedReader(fr);
+			String s;
+			while((s=br.readLine())!=null)
+			{
+				s=s.trim();
+				if(s.length()==1)
+				{
+					words.add(s);
+					continue;
+				}
+				for(int i=0;i<s.length()-1;i++)
+				{
+					if(s.charAt(i)==' ')
+					{
+						continue;
+					}
+					else if(((int)s.charAt(i)>=65 && (int)s.charAt(i)<=90)||(int)s.charAt(i)>=97 && (int)s.charAt(i)<=122)
+					{
+						String s1="";
+						
+						while(((int)s.charAt(i+1)>=65 && (int)s.charAt(i+1)<=90)||((int)s.charAt(i+1)>=97 && (int)s.charAt(i+1)<=122)||((int)s.charAt(i+1)>=48 && (int)s.charAt(i+1)<=57))
+						{
+							if(i<s.length()-2)
+							{
+							s1=s1+s.charAt(i);
+							i=i+1;
+							}
+							
+							else
+							{
+								break;
+							}
+						}
+						s1=s1+s.charAt(i);
+						words.add(s1);
+					}
+					else if(((int)s.charAt(i)>=48 && (int)s.charAt(i)<=57))
+					{
+						String s1="";
+						while(((int)s.charAt(i+1)>=48 && (int)s.charAt(i+1)<=57)&&(i<s.length()-2))
+						{
+							s1=s1+s.charAt(i);
+							i++;
+						}
+						s1=s1+s.charAt(i);
+						words.add(s1);
+					}
+					else if(s.charAt(i)=='\"')
+					{
+						String s1="\"";i++;
+						while(s.charAt(i)!='\"' && i<s.length()-1)
+						{
+							s1=s1+s.charAt(i);
+							i++;
+						}
+						s1=s1+'\"';
+						words.add(s1);
+					}
+					
+					else
+					{
+						String s1="";
+						s1=s1+s.charAt(i);
+						words.add(s1);
+						if(i==s.length()-2)
+						{
+						i++;
+						String s2="";
+						s2=s2+s.charAt(i);
+						words.add(s2);
+						}
+					}
+			}
+			}
+			br.close();
+			fr.close();
+			for(String z:words)
+			{
+				System.out.println(z);
+			}
+					
+	}
+	
+	/*public static void main(String args[]) 
+	{
+		
+		try{
+		lexer ob=new lexer("C:/Users/Vishal/workspace/Interpreter/code.txt");
+		}
+		catch(Exception e)
+		{
+			System.out.println("FUUUCK OFF!!!");
+		}
+	}*/
+}
---
+++ parser.java
@@ -0,0 +1,132 @@
+package com.first;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.first.lexer;
+public class parser extends lexer  {
+	int count=0;int errors=0;String s,s1;
+	String token;
+	ArrayList<String> mutable=new ArrayList<String>();
+	ArrayList<String> immutable=new ArrayList<String>();
+	ArrayList<String> var=new ArrayList<String>();
+	HashMap<String, String> value=new HashMap<String,String>();
+	public parser() throws Exception
+	{
+		super("C:/Users/Vishal/workspace/Interpreter/code.txt");
+		try
+		{
+			lexer ob=new lexer("C:/Users/Vishal/workspace/Interpreter/code.txt");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Fuuuck Offfff!!!");
+		}
+		getToken();
+		parsing();
+	}
+	public void attribute(String s,String s1)
+	{
+		this.s=s;
+		this.s1=s;
+	}
+	public void getToken()throws Exception
+	{
+		String s=words.get(count);
+		count++;
+		token=s;
+	}
+	
+	public void varDeclaration()throws Exception
+	{
+		String v="";
+		if(token.equals("mut"))
+		{
+			getToken();
+			for(String x:keywords)
+			{
+				if(x==token)
+				{
+					System.out.println("ERROR!! : INVALID DECLARATION ");
+					errors++;
+					System.exit(0);
+				}
+			}
+			for(int i=0;i<var.size();i++)
+			{
+				if(token==var.get(i))
+				{
+					System.out.println("ERROR!! : INVALID DECLARATION ");
+				}
+			}
+			v=token;
+			var.add(token);
+			mutable.add(token);
+			getToken();
+			
+		}
+		else
+		{
+			for(String x:keywords)
+			{
+				if(x==token && x!="mut")
+				{
+					System.out.println("ERROR!! : INVALID DECLARATION ");
+					errors++;
+					System.exit(0);
+					
+				}
+			}
+			for(int i=0;i<var.size();i++)
+			{
+				if(token==var.get(i))
+				{
+					System.out.println("ERROR!! : INVALID DECLARATION ");
+				}
+			}
+			v=token;
+			immutable.add(token);
+			var.add(token);
+			
+			getToken();
+		}
+		if(token!=":")
+		{
+			System.out.println(" : Expected");
+			errors++;
+			System.exit(0);
+		}
+		getToken();
+		getToken();
+		getToken();
+		value.put(v,token);
+		
+	}
+	
+	
+	
+	public void parsing() throws Exception
+	{
+		while(count<words.size())
+		{
+			if(token.equals("let"))
+			{
+				getToken();
+				
+				varDeclaration();
+			}
+		}
+	}
+	
+	public static void main(String args[])
+	{
+		try
+		{
+			parser ob1=new parser();
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println("Fuck Off");
+		}
+	}
+}
